aea926dbda9589aa9f7bc5400d8481dc12ff8877
kilo10d <howardchuanwang@gmail.com>
2021-03-03 13:44:03 -0500

    higher frequencies
    shifted 2 octaves

--- JoystickPuzzle/NextClueMusic.cpp
+++ JoystickPuzzle/NextClueMusic.cpp
@@ -1,31 +1,31 @@
 #include "NextClueMusic.h"
 
 void NextClueMusic::playSuccess(){
-	tone(11,330,666);
-	delay(666);
-	tone(11,294,666);
-	delay(666);
-	tone(11,262,666);
-	delay(666);
-	tone(11,440,666);
-	delay(666);
-	tone(11,392,666);
-	delay(666);
-	tone(11,523,666);
-	delay(666);
+	tone(11,NOTE_E6,666);
+	delay(750);
+	tone(11,NOTE_D6,666);
+	delay(750);
+	tone(11,NOTE_C6,666);
+	delay(750);
+	tone(11,NOTE_A6,666);
+	delay(750);
+	tone(11,NOTE_G6,666);
+	delay(750);
+	tone(11,NOTE_C7,666);
+	delay(750);
 }
 
 void NextClueMusic::playFailure(){
-	tone(11,330,666);
-	delay(666);
-	tone(11,311,666);
-	delay(666);
-	tone(11,294,666);
-	delay(666);
-	tone(11,277,666);
-	delay(666);
+	tone(11,NOTE_E6,666);
+	delay(750);
+	tone(11,NOTE_DS6,666);
+	delay(750);
+	tone(11,NOTE_D6,666);
+	delay(750);
+	tone(11,NOTE_CS6,666);
+	delay(750);
 }
 
 void NextClueMusic::playEntryChirp(){
-	tone(11,3000,500);
+	tone(11,8000,500);
 }
