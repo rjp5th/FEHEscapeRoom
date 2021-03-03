9e5a9c44bb879fb5307c6076265c5f3604b7d3c4
kilo10d <howardchuanwang@gmail.com>
2021-03-03 13:36:22 -0500

    e
    e

--- JoystickPuzzle/NextClueMusic.cpp
+++ JoystickPuzzle/NextClueMusic.cpp
@@ -2,19 +2,28 @@
 
 void NextClueMusic::playSuccess(){
 	tone(11,330,666);
+	delay(666);
 	tone(11,294,666);
+	delay(666);
 	tone(11,262,666);
+	delay(666);
 	tone(11,440,666);
+	delay(666);
 	tone(11,392,666);
+	delay(666);
 	tone(11,523,666);
+	delay(666);
 }
 
 void NextClueMusic::playFailure(){
 	tone(11,330,666);
+	delay(666);
 	tone(11,311,666);
+	delay(666);
 	tone(11,294,666);
+	delay(666);
 	tone(11,277,666);
-
+	delay(666);
 }
 
 void NextClueMusic::playEntryChirp(){
