7c080014cf6edcac954d2854d971c5e363f8854a
Robert Pafford <rjpv5th@gmail.com>
2021-03-02 21:39:45 -0500

    Added tone support to code

--- JoystickPuzzle/JoystickPuzzle.ino
+++ JoystickPuzzle/JoystickPuzzle.ino
@@ -1,9 +1,12 @@
 #include "JoystickPasscode.h"
+#include "NextClueMusic.h"
 
 JoystickPasscode *passcodeControl;
+NextClueMusic *toneControl;
 
 void setup() {
   passcodeControl = new JoystickPasscode();
+  toneControl = new NextClueMusic();
   Serial.begin(9600);
   Serial.println("Ready!");
 }
@@ -11,14 +14,17 @@ void setup() {
 void loop() {
   // Wait for input
   passcodeControl->waitAndProcessAction();
+  toneControl->playEntryChirp();
   Serial.println("Item Entered");
 
   // Check if the passcode sequence is complete
   if (passcodeControl->sequenceCompleted()) {
     if (passcodeControl->sequenceCorrect()) {
       Serial.println("Passcode Correct!");
+      toneControl->playSuccess();
     } else {
       Serial.println("Invalid Password");
+      toneControl->playFailure();
     }
     passcodeControl->resetSequence();
   }
