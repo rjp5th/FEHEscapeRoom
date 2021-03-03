f06c3eac05e1ddd2aabbc77890f5d949a3f41102
kilo10d <howardchuanwang@gmail.com>
2021-03-03 13:26:35 -0500

    11 pin
    11 pin

--- JoystickPuzzle/NextClueMusic.cpp
+++ JoystickPuzzle/NextClueMusic.cpp
@@ -1,25 +1,22 @@
 #include "NextClueMusic.h"
 
 void NextClueMusic::playSuccess(){
-	tone.begin(1);
-	tone(330,666);
-	tone(294,666);
-	tone(262,666);
-	tone(440,666);
-	tone(392,666);
-	tone(523,666);
+	tone(11,330,666);
+	tone(11,294,666);
+	tone(11,262,666);
+	tone(11,440,666);
+	tone(11,392,666);
+	tone(11,523,666);
 }
 
 void NextClueMusic::playFailure(){
-	tone.begin(1);
-	tone(330,666);
-	tone(311,666);
-	tone(294,666);
-	tone(277,666);
+	tone(11,330,666);
+	tone(11,311,666);
+	tone(11,294,666);
+	tone(11,277,666);
 
 }
 
 void NextClueMusic::playEntryChirp(){
-	tone.begin(1);
-	tone(3000,500);
+	tone(11,3000,500);
 }
