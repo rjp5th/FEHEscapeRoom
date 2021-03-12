4a84057b635ef050f63f4655a70dea51a05e8c64
kilo10d <howardchuanwang@gmail.com>
2021-03-12 03:07:35 -0500

    l
    l

--- PhotoresistorPuzzle/PhotoresistorPuzzle.ino
+++ PhotoresistorPuzzle/PhotoresistorPuzzle.ino
@@ -1,16 +1,32 @@
 #include "PhotoresistorPasscode.h"
+#include <Servo.h>
 
-#define SERVO_CLOSE_ANGLE 20
-#define SERVO_OPEN_ANGLE 90
+Servo servo;
+
+#define SERVO_CLOSE_ANGLE 120
+#define SERVO_OPEN_ANGLE 10
 
 PhotoresistorPasscode* photoresPasscode;
 
 void setup() {
+  servo.attach(9);
   photoresPasscode = new PhotoresistorPasscode();
   Serial.begin(9600);
   Serial.println("Ready!");
 }
 
 void loop() {
+  photoresPasscode->updateState();
+  if(photoresPasscode->sequenceCompleted())
+  {
+    if(photoresPasscode->sequenceCorrect())
+    {
+      servo.write(SERVO_OPEN_ANGLE);
+      }
+    else
+    {
+      servo.write(SERVO_CLOSE_ANGLE);
+      }
+    }
   
 }
